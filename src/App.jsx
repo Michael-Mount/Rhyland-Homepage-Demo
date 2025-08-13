71e78416950a35e3ac24bba3211120523cbbc6fa
Michael Mount <michaeljoesphmount@gmail.com>
2025-08-13 13:07:31 -0600

    add tailwind and gsap installs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap/gsap-core";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="bg-pink-500">Hello World</h1>
     </>
   );
 }
